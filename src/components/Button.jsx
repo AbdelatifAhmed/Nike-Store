91dc2076df0d9a3046aa0356486cd2b51f16da6e
AbdelatifAhmed <abdoahmed6130@gmail.com>
2024-07-25 17:31:30 +0300

    updates

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,35 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+  fullWidth,
+}) => {
   return (
-    <button className="group flex justify-center items-center 
+    <button
+      className={`
+    group flex justify-center items-center 
     gap-2 px-7 py-4 border font-montserrat text-lg 
-    leading-none bg-coral-red rounded-full transition duration-75
-  text-white border-coral-red hover:border-1 hover:bg-transparent hover:text-coral-red
-   hover:font-semibold focus:ring focus:ring-coral-red">
+    leading-none
+    ${
+      backgroundColor
+        ? `${backgroundColor} ${textColor} ${borderColor}`
+        : "bg-coral-red text-white border-coral-red"
+    }
+    transition duration-75 rounded-full ${fullWidth && 'w-full' }
+    hover:border-1 hover:bg-transparent hover:text-coral-red
+    hover:font-semibold focus:ring focus:ring-coral-red`}
+    >
       {label}
-      <img src={iconURL} alt="Icon" className="ml-2 rounded-full w-5 h-5 group-hover:bg-coral-red 
-      group-hover:border-2 group-hover: border-coral-red" />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="Icon"
+          className="ml-2 rounded-full w-5 h-5 group-hover:bg-coral-red 
+      group-hover:border-2 group-hover: border-coral-red"
+        />
+      )}
     </button>
   );
 };
